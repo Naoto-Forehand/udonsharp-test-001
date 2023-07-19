01c54a4be7b27e73818fdc86e7d2853075af0ddd
Robert <ra.forehand@gmail.com>
2023-07-18 18:07:27 -0700

    Initial work in adding an additional interactive object

---
+++ Assets/UdonSharp/UtilityScripts/FancyWandController.cs
@@ -0,0 +1,38 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common;
+
+public class FancyWandController : UdonSharpBehaviour
+{
+    public GameObject Wand;
+    private Material _wandMaterial;
+    private Color _wandColorDefault;
+    void Start()
+    {
+        if ((Wand != null) && (Wand.activeInHierarchy))
+        {
+            _wandMaterial = Wand.GetComponent<MeshRenderer>().material;
+            _wandColorDefault = _wandMaterial.color;
+        }
+    }
+
+    public override void OnPickupUseUp()
+    {
+        if (Wand.activeInHierarchy)
+        {
+            //Wand.SetActive(false);
+            _wandMaterial.SetColor(Wand.GetInstanceID(), Color.green);
+        }
+    }
+
+    //public override void Interact()
+    //{
+    //    if (Wand.activeInHierarchy)
+    //    {
+    //        Wand.SetActive(false);
+    //    }
+    //}
+}
